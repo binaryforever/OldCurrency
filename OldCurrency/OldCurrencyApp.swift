cca908da74374c74c77adb2b6df7c0f9e31dfcb7
BinaryForever <binaryforever@users.noreply.github.com>
2026-04-10 16:15:37 +0100

    Initial Commit

---
+++ OldCurrency/OldCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldCurrencyApp.swift
+//  OldCurrency
+//
+//  Created by Vincent Holmes on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct OldCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
